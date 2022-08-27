26a3b0c14e2c60b35a19098817a4a1f024c9af53
ThaHobbyist <dipdeys.dey@gmail.com>
2022-08-27 16:17:08 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/auth", userRoutes);
-app.use("/api/message", messagesRoute);
+app.use("api/auth", userRoutes);
+app.use("api/message", messagesRoute);
 
 mongoose
   .connect(process.env.MONGO_URL, {
